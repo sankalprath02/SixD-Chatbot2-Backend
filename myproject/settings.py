0881a09c51847f94a86aef22e6f65fa0a2554434
Sankalp Kumar Rath <sankalprath02@gmail.com>
2025-02-06 18:13:06 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jpoxe%(bd0hd^-rfv#m=^l=!0151mka1py6lhp%p*(e@pzpy#a' 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["SixD-Chatbot2.onrender.com"]
 
 
 # Application definition
